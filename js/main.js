39f26f9a69d65e622e14df80cae2b421fa5f184e
Nadia Pillard <ua9ond@yandex.ru>
2016-11-20 01:24:21 +0700

    carusel&timer added

---
+++ js/main.js
@@ -0,0 +1,53 @@
+'use strict';
+$(document).ready(function() {
+  $('#promo-timer').syotimer({
+      year: 2016,
+      month: 11,
+      day: 25,
+      hour: 20,
+      minute: 30,
+      lang: 'rus',
+      dayVisible: false
+    });
+  
+  //слайдер
+   $('.slider__jcarousel').jcarousel({
+      wrap: 'circular' // Прокрутка по кругу
+    }).jcarouselAutoscroll({
+      interval: (10 * 1000), // 60 * 100 = 1 минута в милисекундах
+      target: '+=1', // На сколько кадров прокручивать за один раз
+      autostart: true
+    });
+  
+  $('.slider__prev')
+      .on('jcarouselcontrol:active', function () {
+        $(this).removeClass('inactive');
+      })
+      .on('jcarouselcontrol:inactive', function () {
+        $(this).addClass('inactive');
+      })
+      .jcarouselControl({
+        target: '-=1'
+      });
+
+    $('.slider__next')
+      .on('jcarouselcontrol:active', function () {
+        $(this).removeClass('inactive');
+      })
+      .on('jcarouselcontrol:inactive', function () {
+        $(this).addClass('inactive');
+      })
+      .jcarouselControl({
+        target: '+=1'
+      });
+
+    $('.slider__jcarousel').hover(function () {
+      $('.jcarousel').jcarouselAutoscroll('stop');
+    }, function () {
+      $('.jcarousel').jcarouselAutoscroll('start');
+    });
+
+  
+  
+  
+})
